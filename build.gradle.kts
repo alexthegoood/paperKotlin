62d78e1a71f86c0a7cb73f981f8377f68e3fcebd
Alexander A. <mail.alexthegoood@gmail.com>
2026-04-04 13:01:23 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
+
+typealias Load = BukkitPluginYaml.PluginLoadOrder
+
+plugins {
+    kotlin("jvm") version libs.versions.kotlin.get()
+    alias(libs.plugins.paperweight.userdev)
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.run.paper)
+    alias(libs.plugins.resource.factory.paper)
+    `maven-publish`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    paperweight.paperDevBundle(libs.versions.paper.api.get())
+    api(libs.bundles.kotlin)
+}
+
+paperPluginYaml {
+    main = "com.alexthegoood.paperKotlin.PaperKotlin"
+    apiVersion = libs.versions.minecraft.get()
+
+    load = Load.STARTUP
+    author = "alexthegoood"
+    description = "Kotlin libraries provider"
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    shadowJar {
+        archiveClassifier = ""
+    }
+
+    runServer {
+        minecraftVersion(libs.versions.minecraft.get())
+        jvmArgs("-Xms2G", "-Xmx2G", "-Dcom.mojang.eula.agree=true")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
+}
---
+++ src/main/kotlin/com/alexthegoood/paperKotlin/PaperKotlin.kt
@@ -0,0 +1,5 @@
+package com.alexthegoood.paperKotlin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PaperKotlin : JavaPlugin()
